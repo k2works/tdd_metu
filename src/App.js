3f3fbe721ee086c1c158f683913e446ca1dbb08a
k2works <kakimomokuri@gmail.com>
2020-10-29 18:55:17 +0900

    refactor: クラスの抽出

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { FizzBuzz } from "./fizzBuzz.js";
+
 export class App {
   constructor() {
     const result = FizzBuzz.generateList();
@@ -40,29 +42,5 @@ export class App {
     };
 
     document.getElementById("app").innerHTML = table(header);
-  }
-}
-
-export class FizzBuzz {
-  static get MAX_NUMBER() {
-    return 100;
-  }
-
-  static generate(number) {
-    const isFizzBuzz = number % 15 === 0;
-    const isFizz = number % 3 === 0;
-    const isBuzz = number % 5 === 0;
-
-    if (isFizzBuzz) return "FizzBuzz";
-    if (isFizz) return "Fizz";
-    if (isBuzz) return "Buzz";
-
-    return number.toString();
-  }
-
-  static generateList() {
-    return [...Array(this.MAX_NUMBER).keys()].map((i) =>
-      FizzBuzz.generate(i + 1)
-    );
   }
 }
