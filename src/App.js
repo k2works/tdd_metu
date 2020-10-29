3dc187f47eb5c0f13d888ec3c8574e208b4455ce
k2works <kakimomokuri@gmail.com>
2020-10-29 18:51:45 +0900

    feat: FizzBuzz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 export class App {
   constructor() {
-    const result = FizzBuzz.generate(15);
+    const result = FizzBuzz.generateList();
     console.log(result);
-    document.getElementById("app").innerHTML = result;
+
+    const header = [...Array(10).keys()]
+      .map((i) => `<th>${i + 1}</th>`)
+      .join("");
+
+    const td = [...Array(10).keys()]
+      .map((i) => (i + 1) * 10)
+      .map((j) =>
+        [...Array(10).keys()]
+          .map((k) => `<td>${FizzBuzz.generate(k + j)}</td>`)
+          .join("")
+      );
+
+    const table = () => {
+      return `
+        <table>
+          <thead>
+          <tr>
+            ${header}
+          </tr>
+          </thead>
+          <tbody>
+            <tr>${td[0]}</t>
+            <tr>${td[1]}</t>
+            <tr>${td[2]}</t>
+            <tr>${td[3]}</t>
+            <tr>${td[4]}</t>
+            <tr>${td[5]}</t>
+            <tr>${td[6]}</t>
+            <tr>${td[7]}</t>
+            <tr>${td[8]}</t>
+            <tr>${td[9]}</t>
+          </tbody>
+        </table>
+        `;
+    };
+
+    document.getElementById("app").innerHTML = table(header);
   }
 }
 
