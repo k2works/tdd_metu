2292adea7401fc08f0b93bcb4c11cb9a1c64891a
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-30 17:59:01 +0900

    refactor: 関数の抽出

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
 import { FizzBuzz } from "./App.js";
 
 describe("FizzBuzz", () => {
+  let fizzBuzz;
+  beforeEach(() => {
+    fizzBuzz = FizzBuzz;
+  });
   it("1を渡したら文字列1を返す", () => {
-    expect(FizzBuzz.generate(1)).toEqual("1");
+    expect(fizzBuzz.generate(1)).toEqual("1");
   });
 
   it("2を渡したら文字列2を返す", () => {
-    expect(FizzBuzz.generate(2)).toEqual("2");
+    expect(fizzBuzz.generate(2)).toEqual("2");
   });
 });
