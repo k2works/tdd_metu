a44e549a8475db902728bf0a24bb39c98e4ae407
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 10:34:38 +0900

    refactor: 関数のインライン化

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 export class App {
   constructor() {
-    const list = FizzBuzz.generateList();
-    const header = [...Array(10).keys()]
-      .map((i) => `<td>${i + 1}</td>`)
-      .join("");
-    const body = [...Array(10).keys()]
-      .map((i) => (i == 0 ? 0 : i * 10))
-      .map((j) =>
-        [...Array(10).keys()].map((k) => `<td>${list[k + j]}</td>`).join("")
-      );
+    const table = (() => {
+      const list = FizzBuzz.generateList();
+      const header = [...Array(10).keys()]
+        .map((i) => `<td>${i + 1}</td>`)
+        .join("");
+      const body = [...Array(10).keys()]
+        .map((i) => (i == 0 ? 0 : i * 10))
+        .map((j) =>
+          [...Array(10).keys()].map((k) => `<td>${list[k + j]}</td>`).join("")
+        );
 
-    const result = `
-    <table>
-      <thead bgcolor="darkgray">
-        <tr>
-        ${header}
-        </tr>
-      </thead>
-      <tbody bgcolor="ivory">
-        <tr>${body[0]}</tr>
-        <tr>${body[1]}</tr>
-        <tr>${body[2]}</tr>
-        <tr>${body[3]}</tr>
-        <tr>${body[4]}</tr>
-        <tr>${body[5]}</tr>
-        <tr>${body[6]}</tr>
-        <tr>${body[7]}</tr>
-        <tr>${body[8]}</tr>
-        <tr>${body[9]}</tr>
-      </tbody>
-    </table>
-    `;
-    document.getElementById("app").innerHTML = result;
+      return `
+      <table>
+        <thead bgcolor="darkgray">
+          <tr>
+          ${header}
+          </tr>
+        </thead>
+        <tbody bgcolor="ivory">
+          <tr>${body[0]}</tr>
+          <tr>${body[1]}</tr>
+          <tr>${body[2]}</tr>
+          <tr>${body[3]}</tr>
+          <tr>${body[4]}</tr>
+          <tr>${body[5]}</tr>
+          <tr>${body[6]}</tr>
+          <tr>${body[7]}</tr>
+          <tr>${body[8]}</tr>
+          <tr>${body[9]}</tr>
+        </tbody>
+      </table>
+      `;
+    })();
+
+    document.getElementById("app").innerHTML = table;
   }
 }
 
