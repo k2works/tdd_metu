3f7c03a0a6cc27b2573ba56747735d8a77bb7c51
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 12:44:34 +0900

    refactor: 関数の抽出

--- api/domain/fizzBuzz.test.ts
+++ api/domain/fizzBuzz.test.ts
@@ -1,12 +1,14 @@
 import { FizzBuzz } from "./fizzBuzz";
 
 describe("FizzBuzz", () => {
+    let fizzBuzz: FizzBuzz
+    beforeEach(() => {
+        fizzBuzz = new FizzBuzz;
+    })
     it("1を渡したら文字列1を返す", () => {
-        const fizzBuzz: FizzBuzz = new FizzBuzz;
         expect(fizzBuzz.generate(1)).toEqual("1")
     })
     it("2を渡したら文字列2を返す", () => {
-        const fizzBuzz: FizzBuzz = new FizzBuzz;
         expect(fizzBuzz.generate(2)).toEqual("2")
     })
 })
