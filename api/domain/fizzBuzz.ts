ffdea83b664d464010cc50a8e44f7fb1c5d48d7d
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 14:05:46 +0900

    refactor: 変数のインライン化

--- api/domain/fizzBuzz.ts
+++ api/domain/fizzBuzz.ts
@@ -1,12 +1,10 @@
 export class FizzBuzz {
   generate(n: number): string {
-    let result: string = n.toString();
-
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
     if (n % 3 === 0) return "Fizz";
     if (n % 5 === 0) return "Buzz";
 
-    return result;
+    return n.toString();
   }
 
   generateList(): string[] {
