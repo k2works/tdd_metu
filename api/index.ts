0ae7801963222914bf6b572bff2e4ea6a57b5834
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 14:28:43 +0900

    feat: FizzBuzzAPI

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,6 @@ import { NowRequest, NowResponse } from "@vercel/node";
 import { FizzBuzz } from "./domain/fizzBuzz";
 
 export default function (req: NowRequest, res: NowResponse) {
-  const fizzBuzz = new FizzBuzz;
-  res.send(fizzBuzz.greeting());
+  const fizzBuzz = new FizzBuzz();
+  res.send(fizzBuzz.generateList());
 }
