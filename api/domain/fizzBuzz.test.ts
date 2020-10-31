13e986dee82b0bec00474f82a36a6ef96580d45e
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 12:53:14 +0900

    refactor: 関数のインライン化

--- api/domain/fizzBuzz.test.ts
+++ api/domain/fizzBuzz.test.ts
@@ -1,20 +1,26 @@
 import { FizzBuzz } from "./fizzBuzz";
 
 describe("FizzBuzz", () => {
-    let fizzBuzz: FizzBuzz
-    beforeEach(() => {
-        fizzBuzz = new FizzBuzz;
-    })
-    it("1を渡したら文字列1を返す", () => {
-        expect(fizzBuzz.generate(1)).toEqual("1")
-    })
-    it("2を渡したら文字列2を返す", () => {
-        expect(fizzBuzz.generate(2)).toEqual("2")
-    })
+  let fizzBuzz: FizzBuzz;
+  beforeEach(() => {
+    fizzBuzz = new FizzBuzz();
+  });
+  describe("三の倍数の場合", () => {
     it("3を渡したら文字列Fizzを返す", () => {
-        expect(fizzBuzz.generate(3)).toEqual("Fizz")
-    })
+      expect(fizzBuzz.generate(3)).toEqual("Fizz");
+    });
+  });
+  describe("五の倍数の場合", () => {
     it("5を渡したら文字列Buzzを返す", () => {
-        expect(fizzBuzz.generate(5)).toEqual("Buzz")
-    })
-})
+      expect(fizzBuzz.generate(5)).toEqual("Buzz");
+    });
+  });
+  describe("その他の場合", () => {
+    it("1を渡したら文字列1を返す", () => {
+      expect(fizzBuzz.generate(1)).toEqual("1");
+    });
+    it("2を渡したら文字列2を返す", () => {
+      expect(fizzBuzz.generate(2)).toEqual("2");
+    });
+  });
+});
--- api/domain/fizzBuzz.ts
+++ api/domain/fizzBuzz.ts
@@ -1,11 +1,11 @@
 export class FizzBuzz {
-    generate(n: number): string {
-        let result: string = n.toString();
-        if (n % 3 === 0) {
-            result = "Fizz"
-        } else if (n % 5 === 0){
-            result = "Buzz"
-        }
-        return result;
+  generate(n: number): string {
+    let result: string = n.toString();
+    if (n % 3 === 0) {
+      result = "Fizz";
+    } else if (n % 5 === 0) {
+      result = "Buzz";
     }
+    return result;
+  }
 }
