62640e15f64a79d8a70b3e1add242bdbdbb92bef
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 14:04:45 +0900

    style: フォーマット

--- api/domain/fizzBuzz.ts
+++ api/domain/fizzBuzz.ts
@@ -1,17 +1,21 @@
 export class FizzBuzz {
   generate(n: number): string {
     let result: string = n.toString();
+
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
     if (n % 3 === 0) return "Fizz";
     if (n % 5 === 0) return "Buzz";
+
     return result;
   }
 
   generateList(): string[] {
     let result: string[] = [];
+
     for (let i: number = 1; i <= 100; i++) {
       result.push(this.generate(i));
     }
+
     return result;
   }
 }
