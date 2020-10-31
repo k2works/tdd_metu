246f68ab6153512dc7e2a2a1a9da742240144064
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 14:27:46 +0900

    refactor: マジックナンバーの置き換え

--- api/domain/fizzBuzz.ts
+++ api/domain/fizzBuzz.ts
@@ -1,19 +1,23 @@
 import { range } from "./utilities";
 
 export class FizzBuzz {
+  static MAX_NUMBER = 100;
+  static FIZZ = "Fizz";
+  static BUZZ = "Buzz";
+
   generate(n: number): string {
     const isFizzBuzz = n % 3 === 0 && n % 5 === 0;
     const isFizz = n % 3 === 0;
     const isBuzz = n % 5 === 0;
 
-    if (isFizzBuzz) return "FizzBuzz";
-    if (isFizz) return "Fizz";
-    if (isBuzz) return "Buzz";
+    if (isFizzBuzz) return `${FizzBuzz.FIZZ}${FizzBuzz.BUZZ}`;
+    if (isFizz) return FizzBuzz.FIZZ;
+    if (isBuzz) return FizzBuzz.BUZZ;
 
     return n.toString();
   }
 
   generateList(): string[] {
-    return range(1, 100).map((i) => this.generate(i));
+    return range(1, FizzBuzz.MAX_NUMBER).map((i) => this.generate(i));
   }
 }
