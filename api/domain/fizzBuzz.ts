0992cb6ae50a8a477bec9df78cd1a37f07b9fb2c
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 14:04:07 +0900

    refactor: ガード節による入れ子の条件記述の置き換え

--- api/domain/fizzBuzz.ts
+++ api/domain/fizzBuzz.ts
@@ -1,13 +1,9 @@
 export class FizzBuzz {
   generate(n: number): string {
     let result: string = n.toString();
-    if (n % 3 === 0 && n % 5 === 0) {
-      result = "FizzBuzz";
-    } else if (n % 3 === 0) {
-      result = "Fizz";
-    } else if (n % 5 === 0) {
-      result = "Buzz";
-    }
+    if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+    if (n % 3 === 0) return "Fizz";
+    if (n % 5 === 0) return "Buzz";
     return result;
   }
 
