2d5ea3ca479763af975c8762e31711eaab90f7d8
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 12:57:31 +0900

    refactor: アルゴリズムの置き換え

--- api/domain/fizzBuzz.ts
+++ api/domain/fizzBuzz.ts
@@ -1,11 +1,10 @@
 export class FizzBuzz {
   generate(n: number): string {
     let result: string = n.toString();
-    if (n % 3 === 0) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      result = "FizzBuzz";
+    } else if (n % 3 === 0) {
       result = "Fizz";
-      if (n % 3 === 0 && n % 5 === 0) {
-        result = "FizzBuzz";
-      }
     } else if (n % 5 === 0) {
       result = "Buzz";
     }
