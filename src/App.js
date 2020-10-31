24527c10dbd241fe4acf9f2534eb582492099216
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-31 09:33:28 +0900

    refactor: マジックナンバーの置き換え

--- src/App.js
+++ src/App.js
@@ -7,19 +7,31 @@ export class App {
 }
 
 export class FizzBuzz {
+  static get MAX_NUMBER() {
+    return 100;
+  }
+  static get FIZZ() {
+    return "Fizz";
+  }
+  static get BUZZ() {
+    return "Buzz";
+  }
+
   static generate(number) {
     const isFizzBuzz = number % 3 === 0 && number % 5 === 0;
     const isFizz = number % 3 === 0;
     const isBuzz = number % 5 === 0;
 
-    if (isFizzBuzz) return "FizzBuzz";
-    if (isFizz) return "Fizz";
-    if (isBuzz) return "Buzz";
+    if (isFizzBuzz) return `${this.FIZZ}${this.BUZZ}`;
+    if (isFizz) return this.FIZZ;
+    if (isBuzz) return this.BUZZ;
 
     return number.toString();
   }
 
   static generateList() {
-    return [...Array(100).keys()].map((i) => FizzBuzz.generate(i + 1));
+    return [...Array(this.MAX_NUMBER).keys()].map((i) =>
+      FizzBuzz.generate(i + 1)
+    );
   }
 }
