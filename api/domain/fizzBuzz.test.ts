36542e35b9408f9d08b8235fea4937daa295dff9
k2works <kakimomokuri@gmail.com>
2020-11-02 10:59:35 +0900

    test: タイプ1

--- api/domain/fizzBuzz.test.ts
+++ api/domain/fizzBuzz.test.ts
@@ -1,50 +1,56 @@
 import { FizzBuzz } from "./fizzBuzz";
 
 describe("FizzBuzz", () => {
-  let fizzBuzz: FizzBuzz;
-  beforeEach(() => {
-    fizzBuzz = new FizzBuzz();
-  });
-  describe("三の倍数の場合", () => {
-    it("3を渡したら文字列Fizzを返す", () => {
-      expect(fizzBuzz.generate(3)).toEqual("Fizz");
+  describe("タイプ1の場合", () => {
+    let fizzBuzz: FizzBuzz;
+    beforeEach(() => {
+      fizzBuzz = new FizzBuzz();
     });
-  });
-  describe("五の倍数の場合", () => {
-    it("5を渡したら文字列Buzzを返す", () => {
-      expect(fizzBuzz.generate(5)).toEqual("Buzz");
+    describe("三の倍数の場合", () => {
+      it("3を渡したら文字列Fizzを返す", () => {
+        expect(fizzBuzz.generate(3)).toEqual("Fizz");
+      });
     });
-  });
-  describe("三と五の倍数の場合", () => {
-    it("15を渡したら文字列FizzBuzzを返す", () => {
-      expect(fizzBuzz.generate(15)).toEqual("FizzBuzz");
+    describe("五の倍数の場合", () => {
+      it("5を渡したら文字列Buzzを返す", () => {
+        expect(fizzBuzz.generate(5)).toEqual("Buzz");
+      });
     });
-  });
-  describe("その他の場合", () => {
-    it("1を渡したら文字列1を返す", () => {
-      expect(fizzBuzz.generate(1)).toEqual("1");
+    describe("三と五の倍数の場合", () => {
+      it("15を渡したら文字列FizzBuzzを返す", () => {
+        expect(fizzBuzz.generate(15)).toEqual("FizzBuzz");
+      });
     });
-    it("2を渡したら文字列2を返す", () => {
-      expect(fizzBuzz.generate(2)).toEqual("2");
+    describe("その他の場合", () => {
+      it("1を渡したら文字列1を返す", () => {
+        expect(fizzBuzz.generate(1)).toEqual("1");
+      });
+      it("2を渡したら文字列2を返す", () => {
+        expect(fizzBuzz.generate(2)).toEqual("2");
+      });
+    });
+
+    describe("1から100までの数の配列を返す", () => {
+      let result: string[];
+      beforeEach(() => {
+        result = fizzBuzz.generateList();
+      });
+      it("配列の初めは文字列の1を返す", () => {
+        expect(result[0]).toEqual("1");
+      });
+      it("配列の最後は文字列のBuzzを返す", () => {
+        expect(result[result.length - 1]).toEqual("Buzz");
+      });
+      it("配列の4番目は文字列のBuzzを返す", () => {
+        expect(result[4]).toEqual("Buzz");
+      });
+      it("配列の14番目は文字列のFizzBuzzを返す", () => {
+        expect(result[14]).toEqual("FizzBuzz");
+      });
     });
   });
 
-  describe("1から100までの数の配列を返す", () => {
-    let result: string[];
-    beforeEach(() => {
-      result = fizzBuzz.generateList();
-    });
-    it("配列の初めは文字列の1を返す", () => {
-      expect(result[0]).toEqual("1");
-    });
-    it("配列の最後は文字列のBuzzを返す", () => {
-      expect(result[result.length - 1]).toEqual("Buzz");
-    });
-    it("配列の4番目は文字列のBuzzを返す", () => {
-      expect(result[4]).toEqual("Buzz");
-    });
-    it("配列の14番目は文字列のFizzBuzzを返す", () => {
-      expect(result[14]).toEqual("FizzBuzz");
-    });
-  });
+  describe("タイプ2の場合", () => {});
+
+  describe("タイプ3の場合", () => {});
 });
