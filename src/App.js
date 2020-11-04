3fec61765498c48ceffd85176ba1edbf5452ca84
k2works <kakimomokuri@gmail.com>
2020-11-04 11:07:55 +0900

    refactor: 関数の抽出

--- src/App.js
+++ src/App.js
@@ -22,87 +22,107 @@ export class App {
   }
 
   renderCounter(initial) {
-    const fetchCounter = (counter) => {
-      const apiUrl = `${this._apiUrl}/counter?number=${counter}`;
-      const fizzBuzzCounter = (resolve, reject) => {
-        fetch(apiUrl)
-          .then((response) => response.json())
-          .then((data) => resolve(data.value))
-          .catch((error) => reject(error));
-      };
-      return new Promise(fizzBuzzCounter);
-    };
+    const component = counterComponent(this._apiUrl, initial);
+    document.getElementById("app-counter").innerHTML = component.contents;
+    document
+      .getElementById("increment")
+      .addEventListener("click", component.incrementEvent);
+    document
+      .getElementById("decrement")
+      .addEventListener("click", component.decrementEvent);
+  }
 
-    let counter = initial;
-    const incrementEvent = async (e) => {
-      counter = counter + 1;
-      document.getElementById("counter").value = await fetchCounter(counter);
-    };
-    const decrementEvent = async (e) => {
-      if (counter === 0) return;
-      counter = counter - 1;
-      document.getElementById("counter").value = await fetchCounter(counter);
-    };
+  renderSelect() {
+    const component = selectComponent(this._apiUrl, this.renderTable);
+    document.getElementById("app-select").innerHTML = component.contents;
+    document
+      .getElementById("selectEvent")
+      .addEventListener("change", component.changeEvent);
+  }
 
-    const contents = `
+  renderTable(list) {
+    const component = tableComponent(list);
+    document.getElementById("app-table").innerHTML = component.contents;
+  }
+}
+
+const counterComponent = (apiUrl, initial) => {
+  const baseApiUrl = apiUrl;
+  let counter = initial;
+
+  const fetchCounter = (counter) => {
+    const apiUrl = `${baseApiUrl}/counter?number=${counter}`;
+    const fizzBuzzCounter = (resolve, reject) => {
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => resolve(data.value))
+        .catch((error) => reject(error));
+    };
+    return new Promise(fizzBuzzCounter);
+  };
+
+  const incrementEvent = async (e) => {
+    counter = counter + 1;
+    document.getElementById("counter").value = await fetchCounter(counter);
+  };
+  const decrementEvent = async (e) => {
+    if (counter === 0) return;
+    counter = counter - 1;
+    document.getElementById("counter").value = await fetchCounter(counter);
+  };
+
+  const contents = `
       <input id="counter" disabled="true" value=${counter}>
       <button id="increment">+</button>
       <button id="decrement">-</button>
     `;
-    document.getElementById("app-counter").innerHTML = contents;
-    document
-      .getElementById("increment")
-      .addEventListener("click", incrementEvent);
-    document
-      .getElementById("decrement")
-      .addEventListener("click", decrementEvent);
-  }
+  return { contents, incrementEvent, decrementEvent };
+};
 
-  renderSelect() {
-    const fetchSelect = (number) => {
-      const apiUrl = `${this._apiUrl}/select?number=${number}`;
-      const fizzBuzzSelect = (resolve, reject) => {
-        fetch(apiUrl)
-          .then((response) => response.json())
-          .then((data) => resolve(data))
-          .catch((error) => reject(error));
-      };
-      return new Promise(fizzBuzzSelect);
+const selectComponent = (apiUrl, renderTable) => {
+  const baseApiUrl = apiUrl;
+  const fetchSelect = (number) => {
+    const apiUrl = `${baseApiUrl}/select?number=${number}`;
+    const fizzBuzzSelect = (resolve, reject) => {
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
     };
+    return new Promise(fizzBuzzSelect);
+  };
 
-    const changeEvent = async (e) => {
-      const number = e.target.value;
-      const list = await fetchSelect(number);
-      this.renderTable(list);
-    };
+  const changeEvent = async (e) => {
+    const number = e.target.value;
+    const list = await fetchSelect(number);
+    renderTable(list);
+  };
 
-    const contents = `
+  const contents = `
     <select id="selectEvent">
       <option value="1">タイプ1</option>
       <option value="2">タイプ2</option>
       <option value="3">タイプ3</option>
     </select>
     `;
-    document.getElementById("app-select").innerHTML = contents;
-    document
-      .getElementById("selectEvent")
-      .addEventListener("change", changeEvent);
-  }
 
-  renderTable(list) {
-    const table = (() => {
-      const header = [...Array(10).keys()]
-        .map((i) => `<td>${i + 1}</td>`)
-        .join("");
-      const body = [...Array(10).keys()]
-        .map((i) => (i == 0 ? 0 : i * 10))
-        .map((j) =>
-          [...Array(10).keys()]
-            .map((k) => `<td>${list[k + j]._value}</td>`)
-            .join("")
-        );
+  return { contents, changeEvent };
+};
 
-      return `
+const tableComponent = (list) => {
+  const table = (() => {
+    const header = [...Array(10).keys()]
+      .map((i) => `<td>${i + 1}</td>`)
+      .join("");
+    const body = [...Array(10).keys()]
+      .map((i) => (i == 0 ? 0 : i * 10))
+      .map((j) =>
+        [...Array(10).keys()]
+          .map((k) => `<td>${list[k + j]._value}</td>`)
+          .join("")
+      );
+
+    return `
             <table>
               <thead bgcolor="darkgray">
                 <tr>
@@ -123,9 +143,8 @@ export class App {
               </tbody>
             </table>
       `;
-    })();
+  })();
 
-    const contents = `<div>${table}</div>`;
-    document.getElementById("app-table").innerHTML = contents;
-  }
-}
+  const contents = `<div>${table}</div>`;
+  return { contents };
+};
