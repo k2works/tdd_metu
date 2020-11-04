30df4829099799a39b69c20da765b58e59ac0802
k2works <kakimomokuri@gmail.com>
2020-11-04 13:56:55 +0900

    feat: 開発を表示する

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { setUp } from "./src/Dev.js";
 let app;
 if (process.env.NODE_ENV === "production") {
   app = new App({ apiUrl: "/api" });
+  setUp();
 } else {
   app = new App({ apiUrl: "http://localhost:3000/api" });
   setUp();
