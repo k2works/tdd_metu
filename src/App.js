c5882dacaf42b47ce4742ad8e054e478569d4626
k2works <kakimomokuri@gmail.com>
2020-11-05 16:53:30 +0900

    refactor(WIP): サブクラスによるタイプコードの置き換え

--- src/App.js
+++ src/App.js
@@ -34,3 +34,15 @@ export class Fibonacci {
     return Math.round(result);
   }
 }
+
+export class FibonacciRecursive {
+  exec(number, memo = []) {
+    if (memo[number]) return memo[number];
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+
+    memo[number] =
+      this.recursive(number - 1, memo) + this.recursive(number - 2, memo);
+    return memo[number];
+  }
+}
