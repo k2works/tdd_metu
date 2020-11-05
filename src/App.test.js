9f4e2c700c37435b5930bb799414fb3d6c349cb9
k2works <kakimomokuri@gmail.com>
2020-11-05 15:35:11 +0900

    refactor: アルゴリズムの変更

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,14 @@
 import { Fibonacci } from "./App.js";
 
 describe("フィボナッチ数列", () => {
-  test("0を渡したら0を返す", () => {
-    expect(Fibonacci.fib(0)).toEqual(0);
-  });
-  test("1を渡したら1を返す", () => {
-    expect(Fibonacci.fib(1)).toEqual(1);
-  });
-  test("2を渡したら1を返す", () => {
-    expect(Fibonacci.fib(2)).toEqual(1);
-  });
-  test("3を渡したら2を返す", () => {
-    expect(Fibonacci.fib(3)).toEqual(2);
-  });
-  test("4を渡したら3を返す", () => {
-    expect(Fibonacci.fib(4)).toEqual(3);
-  });
-  test("5を渡したら5を返す", () => {
-    expect(Fibonacci.fib(5)).toEqual(5);
+  test.each([
+    [0, 0],
+    [1, 1],
+    [2, 1],
+    [3, 2],
+    [4, 3],
+    [5, 5],
+  ])("%iを渡したら%pを返す", (number, expected) => {
+    expect(Fibonacci.fib(number)).toEqual(expected);
   });
 });
