943f93cbcb45681315a590009adfd12c19470c35
k2works <kakimomokuri@gmail.com>
2020-11-05 10:11:40 +0900

    refactor: アルゴリズムの変更

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ export class App {
 export class FizzBuzz {
   static generate(number) {
     let result = number.toString();
-    if (number % 3 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      result = "FizzBuzz";
+    } else if (number % 3 === 0) {
       result = "Fizz";
-      if (number % 3 === 0 && number % 5 === 0) {
-        result = "FizzBuzz";
-      }
     } else if (number % 5 === 0) {
       result = "Buzz";
     }
