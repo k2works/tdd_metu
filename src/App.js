0176b0128563b285af33bd9e6afb6049188f873c
k2works <kakimomokuri@gmail.com>
2020-11-05 16:57:29 +0900

    refactor(WIP): サブクラスによるタイプコードの置き換え

--- src/App.js
+++ src/App.js
@@ -60,3 +60,12 @@ export class FibonacciLoop {
     return c;
   }
 }
+
+export class FibonacciGeneralTerm {
+  exec(number) {
+    let a = ((1 + Math.sqrt(5)) / 2) ** number;
+    let b = ((1 - Math.sqrt(5)) / 2) ** number;
+    const result = (a - b) / Math.sqrt(5);
+    return Math.round(result);
+  }
+}
