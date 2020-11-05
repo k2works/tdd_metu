4be613333e74c8c38149ce8302ec0cd80948bbbc
k2works <kakimomokuri@gmail.com>
2020-11-05 12:46:38 +0900

    test: 明白な実装

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export class App {
 export class Fibonacci {
   static fib(n) {
     if (n === 0) return 0;
-    if (n <= 2) return 1;
+    if (n === 1) return 1;
 
-    return 2;
+    return this.fib(n - 1) + this.fib(n - 2);
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -10,8 +10,13 @@ describe("フィボナッチ数列", () => {
   test("2を渡したら1を返す", () => {
     expect(Fibonacci.fib(2)).toEqual(1);
   });
-
   test("3を渡したら2を返す", () => {
     expect(Fibonacci.fib(3)).toEqual(2);
+  });
+  test("4を渡したら3を返す", () => {
+    expect(Fibonacci.fib(4)).toEqual(3);
+  });
+  test("5を渡したら5を返す", () => {
+    expect(Fibonacci.fib(5)).toEqual(5);
   });
 });
