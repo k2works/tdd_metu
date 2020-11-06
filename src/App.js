bc2d560d989f246261ae1bc607ab3d0141087b49
k2works <kakimomokuri@gmail.com>
2020-11-06 09:52:20 +0900

    test: 仮実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 export class App {
   constructor() {
-    const fib = new Fibonacci(new FibonacciRecursive());
-    document.getElementById("app").innerHTML = fib.exec(40);
+    const contents = `
+    <select>
+      <option>再帰</option>
+      <option>ループ</option>
+      <option>一般項</option>
+    </select>
+    <table>
+      <thead></thead>
+      <thead></thead>
+      <tbody bgcolor="darkgray">
+        <tr>
+          <td>0</td>
+          <td>1</td>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>5</td>
+          <td>8</td>
+          <td>13</td>
+          <td>21</td>
+          <td>34</td>
+        </tr>
+        <tr>
+          <td>55</td>
+          <td>89</td>
+          <td>144</td>
+          <td>233</td>
+          <td>377</td>
+          <td>610</td>
+          <td>987</td>
+          <td>1597</td>
+          <td>2584</td>
+          <td>...</td>
+        </tr>
+      </tbody>
+    </table>
+    `;
+    document.getElementById("app").innerHTML = contents;
   }
 }
 
