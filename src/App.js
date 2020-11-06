7cd2a1caa9e4bc0072937a586bb10630a41f6da7
k2works <kakimomokuri@gmail.com>
2020-11-06 11:20:00 +0900

    feat: 100まで表示する

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import { expect } from "chai";
-
 export class App {
   constructor() {
     this._command = new FibonacciList(new Fibonacci(new FibonacciRecursive()));
-    const list = this._command.exec(100);
+    this._list = this._command.exec(100);
 
-    const select = () => {
-      return `
-        <select>
-          <option>再帰</option>
-          <option>ループ</option>
-          <option>一般項</option>
+    const select = (render, table) => {
+      let list;
+      let command;
+      const changeEvent = (e) => {
+        const value = e.target.value;
+        switch (value) {
+          case "1":
+            command = new FibonacciList(
+              new Fibonacci(new FibonacciRecursive())
+            );
+            list = command.exec(100);
+            render(table(list));
+            break;
+          case "2":
+            command = new FibonacciList(new Fibonacci(new FibonacciLoop()));
+            list = this._command.exec(100);
+            render(table(list));
+            break;
+          case "3":
+            command = new FibonacciList(
+              new Fibonacci(new FibonacciGeneralTerm())
+            );
+            list = this._command.exec(100);
+            render(table(list));
+            break;
+          default:
+            throw "該当するアルゴリズムが存在しません";
+        }
+      };
+
+      const contents = `
+        <select id="app-select">
+          <option value="1">再帰</option>
+          <option value="2">ループ</option>
+          <option value="3">一般項</option>
         </select>
       `;
+
+      return { contents, changeEvent };
     };
 
     const table = (list) => {
@@ -48,13 +77,22 @@ export class App {
       `;
     };
 
+    const renderTable = (contents) => {
+      document.getElementById("app-table").innerHTML = contents;
+    };
+
     const contents = `
       <div>
-      ${select()}
-      ${table(list)}
+      ${select(table).contents}
+      <div id="app-table">
+        ${table(this._list)}
+      </div>
       </div>
     `;
     document.getElementById("app").innerHTML = contents;
+    document
+      .getElementById("app-select")
+      .addEventListener("change", select(renderTable, table).changeEvent);
   }
 }
 
