d3049eaae1d0f050e889c66684813ffbd9fe5be7
k2works <kakimomokuri@gmail.com>
2020-11-06 15:00:34 +0900

    refactor(WIP): サブクラスによるタイプコードの置き換え

--- api/domain/fibonacci.test.ts
+++ api/domain/fibonacci.test.ts
@@ -1,4 +1,4 @@
-import { Fibonacci } from "./fibonacci";
+import { Fibonacci, FibonacciRecursive } from "./fibonacci";
 
 describe("フィボナッチ数列", () => {
   let fib: Fibonacci;
@@ -17,7 +17,8 @@ describe("フィボナッチ数列", () => {
   });
 
   test("大きな数値で計算する(再帰による実装)", () => {
-    expect(fib.recursive(40)).toEqual(102334155);
+    const command = new FibonacciRecursive();
+    expect(command.exec(40)).toEqual(102334155);
   });
   test("大きな数値で計算する(ループによる実装)", () => {
     expect(fib.loop(40)).toEqual(102334155);
--- api/domain/fibonacci.ts
+++ api/domain/fibonacci.ts
@@ -26,3 +26,18 @@ export class Fibonacci {
     return memo[number];
   }
 }
+
+interface Command {
+  exec(number: number): number;
+}
+
+export class FibonacciRecursive implements Command {
+  exec(number: number, memo: number[] = []): number {
+    if (memo[number]) return memo[number];
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+
+    memo[number] = this.exec(number - 1, memo) + this.exec(number - 2, memo);
+    return memo[number];
+  }
+}
