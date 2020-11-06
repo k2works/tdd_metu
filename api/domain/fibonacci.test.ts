82c53efc16c0ac618921e750c3d9591558edbad0
k2works <kakimomokuri@gmail.com>
2020-11-06 14:33:17 +0900

    refactor: 関数の抽出

--- api/domain/fibonacci.test.ts
+++ api/domain/fibonacci.test.ts
@@ -1,24 +1,23 @@
 import { Fibonacci } from "./fibonacci";
 
 describe("フィボナッチ数列", () => {
+  let fib: Fibonacci;
+  beforeEach(() => {
+    fib = new Fibonacci();
+  });
   test("0を渡したら0を返す", () => {
-    const fib = new Fibonacci();
     expect(fib.calc(0)).toEqual(0);
   });
   test("1を渡したら1を返す", () => {
-    const fib = new Fibonacci();
     expect(fib.calc(1)).toEqual(1);
   });
   test("2を渡したら1を返す", () => {
-    const fib = new Fibonacci();
     expect(fib.calc(2)).toEqual(1);
   });
   test("3を渡したら2を返す", () => {
-    const fib = new Fibonacci();
     expect(fib.calc(3)).toEqual(2);
   });
   test("4を渡したら3を返す", () => {
-    const fib = new Fibonacci();
     expect(fib.calc(4)).toEqual(3);
   });
 });
