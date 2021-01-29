4898c58a3ee83f6d4f2478ae8cc8d33f435b88a9
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-01-29 11:51:56 +0900

    refactor: メソッド名の変更

--- api/app.rb
+++ api/app.rb
@@ -8,32 +8,32 @@ class FibonacciTest < Minitest::Test
   def test_fibonacci
     cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
     cases.each do |i|
-      assert_equal i[1], @fib.calc(i[0])
+      assert_equal i[1], @fib.recursive(i[0])
     end
   end
 
   def test_大きな数字_再起処理による実装
-    assert_equal 102_334_155, @fib.calc(40)
+    assert_equal 102_334_155, @fib.recursive(40)
   end
 
   def test_大きな数字_ループ処理による実装
-    assert_equal 102_334_155, @fib.calc2(40)
+    assert_equal 102_334_155, @fib.loop(40)
   end
 
   def test_大きな数字_一般項による実装
-    assert_equal 102_334_155, @fib.calc3(40)
+    assert_equal 102_334_155, @fib.geneeral_term(40)
   end
 end
 
 class Fibonacci
-  def self.calc(number, memo = {})
+  def self.recursive(number, memo = {})
     return 0 if number.zero?
     return 1 if number == 1
 
-    memo[number] ||= calc(number - 1, memo) + calc(number - 2, memo)
+    memo[number] ||= recursive(number - 1, memo) + recursive(number - 2, memo)
   end
 
-  def self.calc2(number)
+  def self.loop(number)
     a = 0
     b = 1
     c = 0
@@ -45,7 +45,7 @@ class Fibonacci
     c
   end
 
-  def self.calc3(number)
+  def self.geneeral_term(number)
     a = ((1 + Math.sqrt(5)) / 2)**number
     b = ((1 - Math.sqrt(5)) / 2)**number
     ((a - b) / Math.sqrt(5)).round
