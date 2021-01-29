954fea8a2f52adeb85c046be62592668deac8cb7
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-01-29 11:23:21 +0900

    refactor: メモ化によるパフォーマンス改善

--- api/app.rb
+++ api/app.rb
@@ -11,13 +11,17 @@ class FibonacciTest < Minitest::Test
       assert_equal i[1], @fib.calc(i[0])
     end
   end
+
+  def test_大きな数字
+    assert_equal 102_334_155, @fib.calc(40)
+  end
 end
 
 class Fibonacci
-  def self.calc(number)
+  def self.calc(number, memo = {})
     return 0 if number.zero?
     return 1 if number == 1
 
-    calc(number - 1) + calc(number - 2)
+    memo[number] ||= calc(number - 1, memo) + calc(number - 2, memo)
   end
 end
