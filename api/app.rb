d669b63d09d839ecf64e21ad2742e38d8cc7e24f
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-01-29 10:48:25 +0900

    refactor: 関数の抽出

--- api/app.rb
+++ api/app.rb
@@ -1,28 +1,32 @@
 require 'minitest/autorun'
 
 class FibonacciTest < Minitest::Test
+  def setup
+    @fib = Fibonacci
+  end
+
   def test_0を渡したら0を返す
-    assert_equal 0, Fibonacci.fib(0)
+    assert_equal 0, @fib.fib(0)
   end
 
   def test_1を渡したら1を返す
-    assert_equal 1, Fibonacci.fib(1)
+    assert_equal 1, @fib.fib(1)
   end
 
   def test_2を渡したら1を返す
-    assert_equal 1, Fibonacci.fib(2)
+    assert_equal 1, @fib.fib(2)
   end
 
   def test_3を渡したら2を返す
-    assert_equal 2, Fibonacci.fib(3)
+    assert_equal 2, @fib.fib(3)
   end
 
   def test_4を渡したら3を返す
-    assert_equal 3, Fibonacci.fib(4)
+    assert_equal 3, @fib.fib(4)
   end
 
   def test_5を渡したら5を返す
-    assert_equal 5, Fibonacci.fib(5)
+    assert_equal 5, @fib.fib(5)
   end
 end
 
