83e1862ba00d8984051ad48c350d802aa2f0e389
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-01-29 09:58:36 +0900

    test: 三角測量

--- api/app.rb
+++ api/app.rb
@@ -4,10 +4,16 @@ class FibonacciTest < Minitest::Test
   def test_0を渡したら0を返す
     assert_equal 0, Fibonacci.fib(0)
   end
+
+  def test_1を渡したら1を返す
+    assert_equal 1, Fibonacci.fib(1)
+  end
 end
 
 class Fibonacci
   def self.fib(n)
-    0
+    return 0 if n.zero?
+
+    1
   end
 end
