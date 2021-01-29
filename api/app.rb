d9ed6423b2a9d37d0014ff4e2f2641ea1a3e7505
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-01-29 10:46:25 +0900

    test: 明白な実装

--- api/app.rb
+++ api/app.rb
@@ -16,13 +16,21 @@ class FibonacciTest < Minitest::Test
   def test_3を渡したら2を返す
     assert_equal 2, Fibonacci.fib(3)
   end
+
+  def test_4を渡したら3を返す
+    assert_equal 3, Fibonacci.fib(4)
+  end
+
+  def test_5を渡したら5を返す
+    assert_equal 5, Fibonacci.fib(5)
+  end
 end
 
 class Fibonacci
   def self.fib(n)
     return 0 if n.zero?
-    return 1 if n <= 2
+    return 1 if n == 1
 
-    2
+    fib(n - 1) + fib(n - 2)
   end
 end
