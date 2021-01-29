a56d9e884d021210044e2522edfd7c9e79b76ce9
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-01-29 11:00:04 +0900

    test: 関数名の変更

--- api/app.rb
+++ api/app.rb
@@ -6,35 +6,35 @@ class FibonacciTest < Minitest::Test
   end
 
   def test_0を渡したら0を返す
-    assert_equal 0, @fib.fib(0)
+    assert_equal 0, @fib.calc(0)
   end
 
   def test_1を渡したら1を返す
-    assert_equal 1, @fib.fib(1)
+    assert_equal 1, @fib.calc(1)
   end
 
   def test_2を渡したら1を返す
-    assert_equal 1, @fib.fib(2)
+    assert_equal 1, @fib.calc(2)
   end
 
   def test_3を渡したら2を返す
-    assert_equal 2, @fib.fib(3)
+    assert_equal 2, @fib.calc(3)
   end
 
   def test_4を渡したら3を返す
-    assert_equal 3, @fib.fib(4)
+    assert_equal 3, @fib.calc(4)
   end
 
   def test_5を渡したら5を返す
-    assert_equal 5, @fib.fib(5)
+    assert_equal 5, @fib.calc(5)
   end
 end
 
 class Fibonacci
-  def self.fib(number)
+  def self.calc(number)
     return 0 if number.zero?
     return 1 if number == 1
 
-    fib(number - 1) + fib(number - 2)
+    calc(number - 1) + calc(number - 2)
   end
 end
